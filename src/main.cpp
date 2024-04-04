429b7464491578989cafea9201e162bd5619a58d
aa20475 <aa20475@gmail.com>
2024-04-04 19:18:20 -0400

    Test Framework base

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,8 @@
 #include "imgui/imgui_impl_opengl3.h"
 #include "imgui/imgui_impl_glfw.h"
 
+#include "tests/test_clear_color.h"
+
 int main(void)
 {
     GLFWwindow *window;
@@ -52,45 +54,9 @@ int main(void)
 
     // Triangle vertices
     {
-        float positions[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f, // 0
-            50.0f, 50.0f, 1.0f, 1.0f,  // 1
-            50.0f, -50.0f, 1.0f, 0.0f, // 2
-            -50.0f, 50.0f, 0.0f, 1.0f // 3
-        };
-
-        unsigned int indices[] = {
-            0, 1, 2,
-            0, 1, 3};
-
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        VertexBufferLayout layout;
-        layout.push<float>(2);
-        layout.push<float>(2);
-        va.addBuffer(vb,layout);
-
-        IndexBuffer ib(indices, 6);
-
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0,0,0));
-        
-
-        Shader shader("res/shaders/texture.shader");
-        shader.bind();
-        shader.setUniform1i("u_Texture", 0);
-
-        Texture texture("res/textures/pikachu.png");
-        texture.bind();
-
-        va.unbind();
-        shader.unbind();
-        vb.unbind();
-        ib.unbind();
 
         Renderer renderer;
 
@@ -100,57 +66,24 @@ int main(void)
         ImGuiIO& io = ImGui::GetIO(); (void)io;
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-
         ImGui_ImplOpenGL3_Init("#version 130");
-        
-        float r = 0.0f;
-        float increment = 0.05f;
-        bool show_demo_window = true;
-        bool show_another_window = false;
-        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-        glm::vec3 translation_a(200,200,0);
-        glm::vec3 translation_b(400,200,0);
+        test::TestClearColor test;
 
         while (!glfwWindowShouldClose(window))
         {
             renderer.clear();
+
+            test.onUpdate(0.0f);
+            test.onRender();
+
             
             ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
-            shader.bind();
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translation_a);
-                glm::mat4 mvp = proj * view * model;
-                shader.setUniformMat4f("u_MVP", mvp);
-                renderer.draw(va, ib, shader);
-            }
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translation_b);
-                glm::mat4 mvp = proj * view * model;
-                shader.setUniformMat4f("u_MVP", mvp);
-                renderer.draw(va, ib, shader);
-            }
 
-            if (r > 1.0f)
-            {
-                increment = -0.05f;
-            }
-            else if (r < 0.0f)
-            {
-                increment = 0.05f;
-            }
-
-            r += increment;
-
-            {
-                static float f = 0.0f;
-                ImGui::SliderFloat3("Translation A", &translation_a.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::SliderFloat3("Translation B", &translation_b.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            }
-
+            test.onImGuiRender();
+            
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
